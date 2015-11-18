f2cc6e0fba3cfc19c515508c2c8c9f3a82be5b71
Ahmed Ali Diriye Aden Hared <ahmed@ahmad.me>
2015-11-18 00:36:10 -0500

    vector code done. look at convert.py for instructions

--- code/config.py
+++ code/config.py
@@ -6,4 +6,4 @@ RESOUCRES = "%s/nlp_resources" % DOCUMENTS
 
 # word2vec
 WORD2VEC_DIR = "%s/word2vec" % RESOUCRES
-FREEBASE_FILE = "%s/knowledge-vectors-skipgram1000.bin" % WORD2VEC_DIR
+FREEBASE_FILE = "%s/freebase-vectors-skipgram1000-en.bin.hdf5" % WORD2VEC_DIR
---
+++ code/convert.py
@@ -0,0 +1,73 @@
+import h5py
+from gensim import utils
+import numpy as np
+from numpy import dtype, fromstring, float32
+
+
+# move this file to ~/Documents/nlp_resource/word2vec/
+# go to that directory and type:
+#
+#    python convert.py
+#
+# it creates the hdf5 file for you in the same directory. make sure
+# the freebase-vectors-skipgram1000-en.bin.gz is in the directory as well!
+# you only need to run this ONCE. it will take nearly an hour.
+#
+# once created, you can use vector.py to access the vectors in a
+# memory effiecient way
+
+
+def main_(filename):
+    fn = filename.split('.')
+    fn[-1] = 'hdf5'
+    hdf5_filename = '.'.join(fn)
+
+    text_f = open(filename, 'r')
+    hdf5_f = h5py.File(hdf5_filename, 'w')
+
+    s = text_f.readline().strip().split()
+
+    while len(s) != 0:
+        word = s[0]
+        vector = np.array([float(num) for num in s[1:]])
+
+        hdf5_f.create_dataset(word, data=vector)
+
+        s = text_f.readline().strip().split()
+
+    text_f.close()
+    hdf5_f.close()
+
+
+def main(filename):
+    fn = filename.split('.')
+    fn[-1] = 'hdf5'
+    hdf5_filename = '.'.join(fn)
+
+    bin_f = utils.smart_open(filename)
+    hdf5_f = h5py.File(hdf5_filename, 'w')
+
+    header = utils.to_unicode(bin_f.readline(), encoding='utf8')
+    vocab_size, vector_size = map(int, header.split())
+    binary_len = dtype(float32).itemsize * vector_size
+    
+    for line_no in xrange(vocab_size):
+        word = []
+
+        while True:
+            ch = bin_f.read(1)
+            if ch == b' ':
+                break
+            if ch != b'\n':
+                word.append(ch)
+
+        word = utils.to_unicode(b''.join(word), encoding='utf8', errors='strict')
+        vector = fromstring(bin_f.read(binary_len), dtype=float32)
+        hdf5_f.create_dataset(word, data=vector)
+
+    bin_f.close()
+    hdf5_f.close()
+
+
+#main_('vectors_wiki.normalized.txt')
+main('freebase-vectors-skipgram1000-en.bin.gz')
