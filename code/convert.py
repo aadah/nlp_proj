2b2f7d132f858641310048eb0320199008aaff2b
Ahmed Ali Diriye Aden Hared <ahmed@ahmad.me>
2015-11-20 15:47:51 -0500

    changed convert.py to fix bug (rerun to get correct hd5f file). added initial proof-of-concept experiments

--- code/convert.py
+++ code/convert.py
@@ -1,42 +1,22 @@
 import h5py
-from gensim import utils
 import numpy as np
 from numpy import dtype, fromstring, float32
+from gensim import utils
+
 
 # move this file to ~/Documents/nlp_resource/word2vec/
 # go to that directory and type:
 #
-#    python convert.py
+#    python convert.py > entities
 #
-# it creates the hdf5 file for you in the same directory. make sure
+# it creates the hdf5 file for you in the same directory. if also creates
+# the entities file, which is required for the model. make sure
 # the freebase-vectors-skipgram1000-en.bin.gz is in the directory as well!
 # you can get it from https://docs.google.com/uc?id=0B7XkCwpI5KDYeFdmcVltWkhtbmM&export=download
 # you only need to run this ONCE. it will take nearly an hour.
 #
 # once created, you can use vector.py to access the vectors in a
 # memory effiecient way
-
-
-def main_(filename):
-    fn = filename.split('.')
-    fn[-1] = 'hdf5'
-    hdf5_filename = '.'.join(fn)
-
-    text_f = open(filename, 'r')
-    hdf5_f = h5py.File(hdf5_filename, 'w')
-
-    s = text_f.readline().strip().split()
-
-    while len(s) != 0:
-        word = s[0]
-        vector = np.array([float(num) for num in s[1:]])
-
-        hdf5_f.create_dataset(word, data=vector)
-
-        s = text_f.readline().strip().split()
-
-    text_f.close()
-    hdf5_f.close()
 
 
 def main(filename):
@@ -63,11 +43,14 @@ def main(filename):
 
         word = utils.to_unicode(b''.join(word), encoding='utf8', errors='strict')
         vector = fromstring(bin_f.read(binary_len), dtype=float32)
-        hdf5_f.create_dataset(word, data=vector)
+
+        if word[:3] == '/en':
+            w = word[4:]
+            hdf5_f.create_dataset(w, data=vector)
+            print w
 
     bin_f.close()
     hdf5_f.close()
 
 
-#main_('vectors_wiki.normalized.txt')
 main('freebase-vectors-skipgram1000-en.bin.gz')
