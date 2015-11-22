f37b75e48fac37468f3acacbed44eb8d882c7916
Jason Tong <jktong@mit.edu>
2015-11-22 16:33:33 -0500

    Add file in resources for entity pairs

--- code/config.py
+++ code/config.py
@@ -11,3 +11,4 @@ FREEBASE_FILE = "%s/freebase-vectors-skipgram1000-en.bin.hdf5" % WORD2VEC_DIR
 
 # reuters
 REUTERS_DIR = "%s/reuters21578" % RESOURCES
+PAIR_FILE = "%s/entity_pairs.txt" % REUTERS_DIR
