bdbb3cb2043286a0b0717402b1e6ddecf05c0464
Jason Tong <jktong@mit.edu>
2015-11-22 15:12:41 -0500

    Add sanity checks and remove OOP components

--- code/parse.py
+++ code/parse.py
@@ -6,18 +6,8 @@ import config
 from ner_recognize import ner_recognize_string
 from cluster import RelationCluster
 
-class Article:
-    
-    def __init__(self, text):
-        self.text = text
-        # body tags cannot be accessed by BeautifulSoup
-        soup = BeautifulSoup(self.text.replace("BODY","CONTENT"), 'lxml')
-        self.body = soup.content
-
-    def get_entity_set(self):
-        return ner_recognize_string(self.body)
-
 def readSgm(fname, entity_set_list):
+    count = 0
     with open(fname, 'r') as f:
         inArticle = False
         text = ''
@@ -26,17 +16,25 @@ def readSgm(fname, entity_set_list):
                 text += line
                 if line.startswith("</REUTERS>"):
                     inArticle = False
-                    a = Article(text)
-                    entity_set_list.append(a.get_entity_set)
+                    soup = BeautifulSoup(text.replace("BODY","CONTENT"), 'lxml')
+                    print type(soup.content.getText())
+                    entity_set_list.append(ner_recognize_string(soup.content.getText()))
+                    count += 1
+                    print '%d articles read' % count
             elif line.startswith("<REUTERS"):
                 inArticle = True
+                text = ''
     return entity_set_list
                     
 def readDir(dirName):
     entity_sets = []
     for fname in os.listdir(dirName):
+        print fname
         if fname.endswith('.sgm'):
             entity_sets += readSgm('%s/%s' %(dirName, fname), [])
+            print entity_sets
+            # for testing, only work with one sgm
+            break
     return entity_sets
 
 def main():
