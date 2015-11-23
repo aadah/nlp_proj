3e5ef826934149be94a8218323b6a3c3fdd0f03b
Jason Tong <jktong@mit.edu>
2015-11-23 15:06:03 -0500

    Add file for storing pairs discovered by the stanford ner method

--- code/config.py
+++ code/config.py
@@ -12,6 +12,7 @@ FREEBASE_FILE = "%s/freebase-vectors-skipgram1000-en.bin.hdf5" % WORD2VEC_DIR
 # reuters
 REUTERS_DIR = "%s/reuters21578" % RESOURCES
 PAIR_FILE = "%s/entity_pairs.txt" % REUTERS_DIR
+STANFORD_PAIRS = "%s/entity_pairs_stanford_1000.txt" % REUTERS_DIR
 
 # stanford
 STANFORD_NER_DIR = "%s/stanford-ner-2014-08-27" % RESOURCES
