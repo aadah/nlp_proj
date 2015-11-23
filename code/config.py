815b8dae0beaa2dcc93bb7b7eb875d651e37664a
Jason Tong <jktong@mit.edu>
2015-11-23 16:31:02 -0500

    Add parameters for experiments in config for convenience

--- code/config.py
+++ code/config.py
@@ -12,11 +12,12 @@ FREEBASE_FILE = "%s/freebase-vectors-skipgram1000-en.bin.hdf5" % WORD2VEC_DIR
 # reuters
 REUTERS_DIR = "%s/reuters21578" % RESOURCES
 PAIR_FILE = "%s/entity_pairs.txt" % REUTERS_DIR
-STANFORD_PAIRS = "%s/entity_pairs_stanford_1000.txt" % REUTERS_DIR
+REUTERS_PAIRS = "%s/reuters_entity_pairs_stanford_1000.txt" % REUTERS_DIR
 
 # bbc
 BBC_DIR = "%s/bbc" % RESOURCES
 BBC_ARTICLES = "%s/links.txt" % BBC_DIR
+BBC_PAIRS = "%s/entity_pairs.txt" % BBC_DIR
 
 # stanford
 STANFORD_NER_DIR = "%s/stanford-ner-2014-08-27" % RESOURCES
@@ -26,3 +27,8 @@ STANFORD_3CLASS = '%s/english.all.3class.distsim.crf.ser.gz' % STANFORD_NER_MODE
 STANFORD_4CLASS = '%s/english.conll.4class.distsim.crf.ser.gz' % STANFORD_NER_MODELS_DIR
 STANFORD_7CLASS = '%s/english.muc.7class.distsim.crf.ser.gz' % STANFORD_NER_MODELS_DIR
 
+# parameters
+NER_MODE = 'stanford'
+ARTICLE_SOURCE = 'bbc' # can be 'reuters'
+STANFORD_MODEL_NUM = 3
+PAIR_OUTPUT_FILE = BBC_PAIRS
