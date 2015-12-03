97c35eac96f44ad40387114e3e2b2afb71a5dbf5
Emily Kellison-Linn <ekl@mit.edu>
2015-12-02 20:54:24 -0500

    code to save and load SVM model

--- code/svm_nn_classify.py
+++ code/svm_nn_classify.py
@@ -1,6 +1,12 @@
 import sklearn.svm
 import numpy as np
 
+from sklearn.externals import joblib
+
+from keras.models import Sequential
+from keras.layers.core import Dense, Dropout, Activation
+from keras.optimizers import SGD
+
 def get_data(train_size=100, cv_size=50, test_size=50):
 ##    print "starting to load data"
 ##    all_data = np.loadtxt("dummy_data/gisette_train.data.txt")
@@ -16,8 +22,15 @@ def get_data(train_size=100, cv_size=50, test_size=50):
            all_data[train_size:train_size+test_size,:num_features], all_labels[train_size:train_size+test_size], \
            all_data[train_size+test_size:,:num_features], all_labels[train_size+test_size:]
 
+def create_svm(my_c, my_gamma):
 
-def cross_validate_svc(train_set, train_labels, cv_set, cv_labels):
+    return sklearn.svm.SVC(C=my_c, kernel='rbf', degree=3,
+                             gamma=my_gamma, coef0=0.0, shrinking=True,
+                             probability=False, tol=0.001, cache_size=200,
+                             class_weight=None, verbose=False, max_iter=-1,
+                             decision_function_shape=None, random_state=None) 
+
+def cross_validate_svm(train_set, train_labels, cv_set, cv_labels):
     # grid search over c and gamma
     print "beginning cross-validation"
     c_exp_range = range(-3,4) #range(-5,16,2)
@@ -28,11 +41,7 @@ def cross_validate_svc(train_set, train_labels, cv_set, cv_labels):
     my_iter = 0
     for c_exp in c_exp_range:
         for gamma_exp in gamma_exp_range:
-            my_svc = sklearn.svm.SVC(C=10**c_exp, kernel='rbf', degree=3,
-                             gamma=10**gamma_exp, coef0=0.0, shrinking=True,
-                             probability=False, tol=0.001, cache_size=200,
-                             class_weight=None, verbose=False, max_iter=-1,
-                             decision_function_shape=None, random_state=None)
+            my_svc = create_svm(10**c_exp, 10**gamma_exp)
             my_svc.fit(train_set, train_labels)
             result = my_svc.score(cv_set, cv_labels)
             results[c_iter, gamma_iter] = result
@@ -48,19 +57,45 @@ def cross_validate_svc(train_set, train_labels, cv_set, cv_labels):
     print "best c:", best_c, ". best gamma:", best_gamma
     return best_c, best_gamma
 
-def cross_validate_nn():
-    return
+def create_nn(train_data, train_labels, test_data, test_labels):
+    model = Sequential()
+    # Dense(64) is a fully-connected layer with 64 hidden units.
+    # in the first layer, you must specify the expected input data shape:
+    # here, 20-dimensional vectors.
+    model.add(Dense(64, input_dim=train_data.shape[0], init='uniform'))
+    model.add(Activation('tanh'))
+    model.add(Dropout(0.5))
+    model.add(Dense(64, init='uniform'))
+    model.add(Activation('tanh'))
+    model.add(Dropout(0.5))
+    model.add(Dense(2, init='uniform'))
+    model.add(Activation('softmax'))
 
-    
+ #  sgd = SGD(lr=0.1, decay=1e-6, momentum=0.9, nesterov=True)
+    model.compile(loss='mean_squared_error', optimizer='sgd')
+    print "compiled model"
+    model.fit(train_data, train_labels, nb_epoch=20, batch_size=16)
+    score = model.evaluate(test_data, test_labels, batch_size=16)
+    print score
 
-if __name__ == "__main__":
 
-    np.set_printoptions(precision=4)
+def make_and_save_svm(filename):
 
     train_set, train_labels, \
-               cv_set, cv_labels, \
-               test_set, test_labels = get_data()
+        cv_set, cv_labels, \
+        test_set, test_labels = get_data()
 
-    best_c, best_gamma = cross_validate_svc(train_set, train_labels, cv_set, cv_labels)
+    best_c, best_gamma = cross_validate_svm(train_set, train_labels, cv_set, cv_labels)
 
+    my_svc = create_svm(best_c, best_gamma)
+    my_svc.fit(train_set, train_labels)
+    joblib.dump(my_svc, filename)
+
+def load_svm(filename):
+    return joblib.load(filename)
+
+def fit_svm(test_data, filename):
+    svm = load_svm(filename)
+    labels = svm.fit(test_data)
+    return labels
         
