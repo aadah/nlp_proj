e82f838b2e782c671c0fbfd4d903582cadcb899c
Ahmed Ali Diriye Aden Hared <ahmed@ahmad.me>
2015-12-12 04:44:05 -0500

    new data code

--- code/config.py
+++ code/config.py
@@ -37,6 +37,8 @@ STANFORD_PARSER_MODEL = "%s/stanford-parser-3.5.2-models.jar" % STANFORD_PARSER_
 # data
 RAW_DATA_TXT = '%s/raw_data.txt' % RESOURCES
 NEW_DATA_TXT = '%s/new_data.txt' % RESOURCES
+NEW_DATA_TRAIN_NPY = '%s/new_data_train.npy' % RESOURCES
+NEW_DATA_TEST_NPY = '%s/new_data_test.npy' % RESOURCES
 
 RELATIONS_DICT = '%s/relations_dictionary.txt' % RESOURCES
 SINGLE_INSTANCE_TRAIN = '%s/single_instance_train.npy' % RESOURCES
--- code/generate.py
+++ code/generate.py
@@ -1,4 +1,5 @@
 import os.path
+import random
 import numpy as np
 from pprint import pprint
 
@@ -9,6 +10,7 @@ import config
 
 def make_instance_instance_txt():
     np.random.seed(1000)
+    random.seed(1000)
 
     wd = sparql.WikiDataClient()
     vm = vector.MIDVectorModel()
@@ -67,13 +69,67 @@ def make_instance_instance_txt():
                   int(rel_j[1:]))
         neg.append(neg_ex)
 
-    examples = pos_ex + neg_ex
+    examples = pos + neg
 
     with open(config.NEW_DATA_TXT, 'w') as f:
-        pprint(examples, stream=f)
+        for ex in examples:
+            f.write(repr(ex)+'\n')
 
     vm.close()
 
+
+def make_instance_instance_npy():
+    vm = vector.MIDVectorModel()
+    pos = []
+    neg = []
+
+    split = 0.05
+
+    print 'loading . . .'
+    with open(config.NEW_DATA_TXT) as f:
+        data = map(eval, f.readlines())
+
+    print 'converting . . .'
+    for datum in data:
+        a = vm[datum[0][1]]
+        b = vm[datum[0][3]]
+        c = vm[datum[1][1]]
+        d = vm[datum[1][3]]
+        l1 = np.array([datum[2]])
+        l2 = np.array([datum[3]])
+        vec = np.hstack([a,b,c,d,l1,l2])
+
+        if l1 == l2:
+            pos.append(vec)
+        else:
+            neg.append(vec)
+
+    print 'shuffling . . .'
+    random.shuffle(pos)
+    random.shuffle(neg)
+
+    print 'building . . .'
+    num_pos = len(pos)
+    num_neg = len(neg)
+    num_train_pos = num_pos*(1-split)
+    num_train_neg = num_neg*(1-split)
+    
+    train_pos = pos[:num_train_pos]
+    train_neg = neg[:num_train_neg]
+    test_pos = pos[num_train_pos:]
+    test_neg = neg[num_train_neg:]
+
+    train = np.vstack(train_pos + train_neg)
+    test = np.vstack(test_pos + test_neg)
+
+    print 'train:', train.shape
+    print 'test:', test.shape
+
+    print 'saving . . .'
+    np.save(config.NEW_DATA_TRAIN_NPY, train)
+    np.save(config.NEW_DATA_TEST_NPY, test)
+
+    vm.close()
 
 ###########################################################
 
